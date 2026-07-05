d02c1fb3b2e79679f6ee3711db260019589ea9c3
marvcomps26 <marvcomps26@gmail.com>
2026-07-05 23:25:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-function loadPage(url){
-  document.getElementById("appFrame").src = url;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash");
 
-setTimeout(() => {
-  document.getElementById("splash").style.display = "none";
-}, 1200);
+  setTimeout(() => {
+    if (splash) {
+      splash.style.opacity = "0";
+      splash.style.pointerEvents = "none";
+    }
+  }, 1200);
+
+  setTimeout(() => {
+    if (splash) {
+      splash.style.display = "none";
+    }
+  }, 1700);
+});
