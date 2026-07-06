a68a1da6c107b8c194658d9c141a86a59cc1f335
marvcomps26 <marvcomps26@gmail.com>
2026-07-06 12:42:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
     if (splash) splash.classList.add("hide");
   }, 1100);
 });
+// DAILY CODE POPUP
 
+function showDailyCode() {
+  document.getElementById("dailyCodePopup").classList.add("show");
+}
+
+function closeDailyCode() {
+  document.getElementById("dailyCodePopup").classList.remove("show");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".gold-button");
+
+  if (btn) {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      showDailyCode();
+    });
+  }
+});
