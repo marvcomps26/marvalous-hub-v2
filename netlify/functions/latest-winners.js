6b7cc2622b2fc3c846b872a53034e91ed1d71088
marvcomps26 <marvcomps26@gmail.com>
2026-07-08 22:18:32 +0100

    Update latest-winners.js

--- netlify/functions/latest-winners.js
+++ netlify/functions/latest-winners.js
@@ -1,1 +1,30 @@
+exports.handler = async function () {
+  try {
+    const res = await fetch("https://www.marvalouscompetitions.co.uk/instant-winners");
+    const html = await res.text();
 
+    const clean = html
+      .replace(/<script[\s\S]*?<\/script>/gi, "")
+      .replace(/<style[\s\S]*?<\/style>/gi, "")
+      .replace(/<[^>]+>/g, " ")
+      .replace(/\s+/g, " ")
+      .trim();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        text: clean.slice(0, 900)
+      })
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not load winners" })
+    };
+  }
+};
