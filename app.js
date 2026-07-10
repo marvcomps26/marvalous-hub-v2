05c1a9023630b3240cc685fd0fc48d2777b2440e
marvcomps26 <marvcomps26@gmail.com>
2026-07-10 16:10:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ function submitLeagueSignup(){
     const elapsed = Math.min((currentTime - lastTime) / 1000, 0.05);
     lastTime = currentTime;
 
-    position -= 18 * elapsed;
+    position -= 25 * elapsed;
 
     if (groupWidth && position <= -groupWidth) {
       position += groupWidth;
